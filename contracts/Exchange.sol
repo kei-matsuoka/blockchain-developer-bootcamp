b3fb8d7883a5c0856b81758aa4fb79e465121518
kei-matsuoka <keimatsuoka0311@gmail.com>
2023-04-10 18:08:52 +0900

    12. Cancel Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -11,13 +11,9 @@ contract Exchange {
 
     mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order) public orders;
+    mapping(uint256 => bool) public orderCancelled;
 
-    event Deposit(
-        address token,
-        address user,
-        uint256 amount,
-        uint256 balance
-    );
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
 
     event Withdraw(
         address token,
@@ -27,6 +23,16 @@ contract Exchange {
     );
 
     event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+    event Cancel(
         uint256 id,
         address user,
         address tokenGet,
@@ -101,6 +107,25 @@ contract Exchange {
             _amountGet,
             _tokenGive,
             _amountGive,
+            block.timestamp
+        );
+    }
+
+    function cancelOrder(uint256 _id) public {
+        _Order storage _order = orders[_id];
+
+        require(address(_order.user) == msg.sender);
+        require(_order.id == _id);
+        
+        orderCancelled[_id] = true;
+
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
             block.timestamp
         );
     }
