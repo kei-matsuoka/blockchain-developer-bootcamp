6da97fec0b96ff33adc9c16e10137ed50428ab21
kei-matsuoka <keimatsuoka0311@gmail.com>
2023-04-13 10:29:42 +0900

    17. Redux Pt II

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,30 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import config from '../config.json';
 
-import { loadProvider, loadNetwork, loadAccount, loadToken } from '../store/interactions';
+import { 
+  loadProvider,
+  loadNetwork,
+  loadAccount,
+  loadTokens,
+  loadExchange
+ } from '../store/interactions';
 
 function App() {
 
   const dispatch = useDispatch()
 
   const loadBlockchainData = async ()=>{
-    await loadAccount(dispatch)
-
     const provider = loadProvider(dispatch)
     const chainId = await loadNetwork(provider, dispatch)
+    
+    await loadAccount(provider, dispatch)
 
-    await loadToken(provider, config[chainId].DApp.address, dispatch)
+    const DApp = config[chainId].DApp
+    const mETH = config[chainId].mETH
+    await loadTokens(provider, [DApp.address, mETH.address], dispatch)
+
+    const exchangeConfig = config[chainId].exchange
+    await loadExchange(provider, exchangeConfig.address, dispatch)
   }
 
   useEffect(() => {
--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { provider, tokens } from './reducers';
+import { provider, tokens, exchange } from './reducers';
 
 const reducer = combineReducers({
-  provider, tokens
+  provider, tokens, exchange
 })
 
 const initialState = {
